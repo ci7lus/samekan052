d56ce5dbfc15e6bfb22069a7e3668feced21ff9f
ci7lus <7887955+ci7lus@users.noreply.github.com>
2020-06-29 23:11:33 +0900

    :art: ファイル分割

--- dev.py
+++
@@ -1,5 +0,0 @@
-from index import app
-
-app.debug = True
-app.env = "DEV"
-app.run()
--- index.py
+++ index.py
@@ -1,20 +1,22 @@
 from flask import Flask, render_template, jsonify
-import markovify
-import MeCab
+from flask_cors import CORS
+from makeSentence import makeSentence
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
 def index():
-    with open("dist/model.json", "r") as f:
-        textModel = markovify.Text.from_json(f.read())
+    return render_template("index.html", sentence=makeSentence())
 
-    while True:
-        made = textModel.make_sentence(tries=100)
 
-        if made:
-            sentence = "".join(made.split())
-            break
+@app.route("/api")
+def api():
+    return jsonify(sentence=makeSentence())
 
-    return render_template("index.html", sentence=sentence)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.env = "DEV"
+    app.run()

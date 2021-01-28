a55d9e0d87c843289a9e1f1713b4df9c9c976530
Nep <spica@starry.blue>
2021-01-28 09:03:44 +0000

    🐲 MeCab 辞書のパスを変更可能にした

--- testMecab.py
+++ testMecab.py
@@ -1,5 +1,7 @@
+import os
 import MeCab
 
-m = MeCab.Tagger("-d /usr/lib64/mecab/dic/mecab-ipadic-neologd -Owakati")
+dict_path = os.getenv("MECAB_DICTIONARY_PATH", "/usr/lib64/mecab/dic/mecab-ipadic-neologd")
+m = MeCab.Tagger(f"-d {dict_path} -Owakati")
 
 print(m.parse("彼女はペンパイナッポーアッポーペンと恋ダンスを踊った。"))
